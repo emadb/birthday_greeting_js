1497ec84e0d03b51f39f9afe5490fa5e8a98fb76
ema <emanuele@codiceplastico.com>
2020-02-22 22:58:00 +0100

    empty project

--- src/runner.js
+++
@@ -1,9 +0,0 @@
-
-const main = async (fileName, today, repository, mailer) => {
-
-  const employees = await repository.loadAll(fileName)
-  return Promise.all(employees.filter(e => e.isBirthday(today)).map(e => mailer.send(e)))
-      
-}
-
-module.exports = main
